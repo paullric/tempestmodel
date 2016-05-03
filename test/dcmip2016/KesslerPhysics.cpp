dfb03936db87af0bf69b7bf9c06628ce2008d8b9
Paul Ullrich <paullrich@ucdavis.edu>
2016-05-02 21:43:14 -0700

    - Clean up formatting in KesslerPhysics.cpp

--- test/dcmip2016/KesslerPhysics.cpp
+++ test/dcmip2016/KesslerPhysics.cpp
@@ -75,55 +75,55 @@ WorkflowProcess(
 	
 ///////////////////////////////////////////////////////////////////////////////
 	
-	void KesslerPhysics::Perform(
-		const Time & time
+void KesslerPhysics::Perform(
+	const Time & time
 ) {
-		// Indices of EquationSet variables
-		const int UIx = 0;
-		const int VIx = 1;
-		const int TIx = 2;
-		const int WIx = 3;
-		const int RIx = 4;
+	// Indices of EquationSet variables
+	const int UIx = 0;
+	const int VIx = 1;
+	const int TIx = 2;
+	const int WIx = 3;
+	const int RIx = 4;
 
-		// Get DeltaT
-		double dDeltaT = m_timeFrequency.GetSeconds();
+	// Get DeltaT
+	double dDeltaT = m_timeFrequency.GetSeconds();
 		
-		// Get a copy of the GLL grid
-		Grid * pGrid = m_model.GetGrid();
+	// Get a copy of the GLL grid
+	Grid * pGrid = m_model.GetGrid();
 
-		// Check position of variables
-		if (pGrid->GetVarLocation(TIx) == DataLocation_REdge) {
-			_EXCEPTIONT("Not implemented for --vstagger CPH, use --vstagger LOR");
-		}
+	// Check position of variables
+	if (pGrid->GetVarLocation(TIx) == DataLocation_REdge) {
+		_EXCEPTIONT("Not implemented for --vstagger CPH, use --vstagger LOR");
+	}
 
-		// Physical constants
-		const PhysicalConstants & phys = m_model.GetPhysicalConstants();
-	
-		// Number of radial elements
-		int nz = pGrid->GetRElements();
+	// Physical constants
+	const PhysicalConstants & phys = m_model.GetPhysicalConstants();
 
-		// Perform local update
-		for (int n = 0; n < pGrid->GetActivePatchCount(); n++) {
+	// Number of radial elements
+	int nz = pGrid->GetRElements();
+
+	// Perform local update
+	for (int n = 0; n < pGrid->GetActivePatchCount(); n++) {
 		GridPatch * pPatch = pGrid->GetActivePatch(n);
-		
+
 		const PatchBox & box = pPatch->GetPatchBox();
-		
+
 		// Get latitude
 		const DataArray2D<double> & dataLatitude = pPatch->GetLatitude();
-		
+
 		// Grid data
 		DataArray4D<double> & dataNode =
 		pPatch->GetDataState(0, DataLocation_Node);
-		
+
 		DataArray4D<double> & dataTracer =
 		pPatch->GetDataTracers(0);
-		
-	   const DataArray3D<double> & dataZLevels	= pPatch->GetZLevels();
-		
+
+		const DataArray3D<double> & dataZLevels = pPatch->GetZLevels();
+
 		// Loop over all horizontal nodes in GridPatch
 		for (int i = box.GetAInteriorBegin(); i < box.GetAInteriorEnd(); i++) {
 		for (int j = box.GetBInteriorBegin(); j < box.GetBInteriorEnd(); j++) {
-	 
+
 			// Loop over all levels in column
 			for (int k = 0; k < pGrid->GetRElements(); k++) {
 
@@ -146,7 +146,7 @@ WorkflowProcess(
 
 				// Pointwise virtual temperature
 				double dTv = dPressure / (dRho * phys.GetR());
-	
+
 				// Water vapor (RhoQv / Rho)
 				qv[k] = dataTracer[0][k][i][j] / dataNode[RIx][k][i][j];
 
@@ -165,7 +165,7 @@ WorkflowProcess(
 				// Heights of each level
 				zc[k] = dataZLevels[k][i][j];
 	
-				// Exner function (Tv/theta)
+				// Exner function (Tv/thetav)
 				pk[k] = dTv / dThetav;
 			}
 
@@ -200,7 +200,7 @@ WorkflowProcess(
 		}
 		}
 	}
-	
+
 	// Call up the stack to update performance time
 	WorkflowProcess::Perform(time);
 }
