3a12877cfb557c0d455e24927a5a336723998755
Jorge E Guerra <jeguerra@ucdavis.edu>
2017-08-01 16:46:24 -0700

    Fix previous.

--- src/atm/VerticalDynamicsFEM.cpp
+++ src/atm/VerticalDynamicsFEM.cpp
@@ -3011,8 +3011,8 @@ void VerticalDynamicsFEM::BuildF(
 			for (int k = 0; k <= nRElements; k++) {
 				dF[VecFIx(FIxFromCIx(c), k)] -=
 					m_dResidualAuxREdge[k]
-					/ dZtop
-					/ dZtop
+					/ pGrid->GetZtop()
+					/ pGrid->GetZtop()
 					* m_dDiffDiffStateHypervis[c][k];
 			}
 
@@ -3031,8 +3031,8 @@ void VerticalDynamicsFEM::BuildF(
 			for (int k = 0; k < nRElements; k++) {
 				dF[VecFIx(FIxFromCIx(c), k)] -=
 					m_dResidualAuxNode[k]
-					/ dZtop
-					/ dZtop
+					/ pGrid->GetZtop()
+					/ pGrid->GetZtop()
 					* m_dDiffDiffStateHypervis[c][k];
 			}
 		}
