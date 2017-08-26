3fd8ff59efd12534bfadbc1073c35cce07bebc23
Jorge E Guerra <jeguerra@ucdavis.edu>
2017-08-26 13:16:06 -0700

    Rayleigh layer in vertical dynamics without density.

--- src/atm/VerticalDynamicsFEM.cpp
+++ src/atm/VerticalDynamicsFEM.cpp
@@ -1338,16 +1338,21 @@ void VerticalDynamicsFEM::ApplyRayleighFriction(
 	if ((nEqSet == EquationSet::PrimitiveNonhydrostaticEquations) && !fCartXZ) {
 		nEffectiveC[0] = 0; nEffectiveC[1] = 1;
 		nEffectiveC[2] = 2; nEffectiveC[3] = 3;
-		nEffectiveC[4] = 4;
+		//nEffectiveC[4] = 4;
+		nEffectiveC[nComponents - 1] = 0;
+		nComponents = nComponents - 1;
 	}
 	// 2D Cartesian XZ primitive nonhydro models with no density treatment
 	else if ((nEqSet == EquationSet::PrimitiveNonhydrostaticEquations) && fCartXZ) {
 		nEffectiveC[0] = 0;
 		nEffectiveC[1] = 2;
 		nEffectiveC[2] = 3;
-		nEffectiveC[3] = 4;
+		//nEffectiveC[3] = 4;
+		//nEffectiveC[nComponents - 1] = 0;
+		//nComponents = nComponents - 1;
+		nEffectiveC[nComponents - 2] = 0;
 		nEffectiveC[nComponents - 1] = 0;
-		nComponents = nComponents - 1;
+		nComponents = nComponents - 2;
 	}
 	// Other model types (advection, shallow water, mass coord)
 	else {
